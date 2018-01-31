c208bec15570c9a29c55dd9135d680110cf6d695
Memduh Çağrı Demir <memduhcagridemir@gmail.com>
2018-01-31 22:54:07 +0300

    Tensorboard added

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from Dataset import Data_i2b2
 
 
 data_i2b2 = Data_i2b2()
-data_i2b2 = data_i2b2.load_data()
-data_i2b2 = data_i2b2.save()
-# data_i2b2 = data_i2b2.load()
+# data_i2b2 = data_i2b2.load_data()
+# data_i2b2 = data_i2b2.save()
+data_i2b2 = data_i2b2.load()
 data_i2b2.generate_vocabulary()
 
 train_batcher = Batcher(data_i2b2.train_data, data_i2b2.vocabulary_enumerated)
--- model.py
+++ model.py
@@ -1,58 +1,60 @@
+import datetime
+
 import tensorflow as tf
 from tensorflow.contrib import rnn
 
+
 class Model(object):
     def __init__(self, train_batcher=None, test_batcher=None):
-        self.batcher = train_batcher
-        self.test_batcher = test_batcher
+        self.number_of_hidden = 256
         self.number_of_timesteps = 9
         self.number_of_features = 551
+        self.batcher = train_batcher
+        self.test_batcher = test_batcher
 
         self.graph = tf.Graph()
         with self.graph.as_default():
-            self.x = tf.placeholder(tf.float32, [None, self.number_of_timesteps, self.number_of_features])
-            self.y = tf.placeholder(tf.float32, [None, 2])
+            with tf.name_scope('input'):
+                self.x = tf.placeholder(tf.float32, [None, self.number_of_timesteps, self.number_of_features])
 
-            number_of_hidden = 256
-            w = { "out": tf.Variable(tf.random_normal([2 * number_of_hidden, 2])) }
-            b = { "out": tf.Variable(tf.random_normal([2])) }
+            with tf.name_scope('target'):
+                self.y = tf.placeholder(tf.float32, [None, 2])
 
-            def BiRNN(x, weights, biases):
-                # Prepare data shape to match `rnn` function requirements
-                # Current data input shape: (batch_size, timesteps, n_input)
-                # Required shape: 'timesteps' tensors list of shape (batch_size, num_input)
+            with tf.name_scope('weights'):
+                w1 = tf.Variable(tf.random_normal([2 * self.number_of_hidden, 2]))
 
-                # Unstack to get a list of 'timesteps' tensors of shape (batch_size, num_input)
-                x = tf.unstack(x, self.number_of_timesteps, 1)
+            with tf.name_scope('biases'):
+                b1 = tf.Variable(tf.random_normal([2]))
 
-                # Define lstm cells with tensorflow
-                # Forward direction cell
-                lstm_fw_cell = rnn.BasicLSTMCell(number_of_hidden, forget_bias=1.0)
-                # Backward direction cell
-                lstm_bw_cell = rnn.BasicLSTMCell(number_of_hidden, forget_bias=1.0)
+            with tf.name_scope('operations'):
+                x = tf.unstack(self.x, self.number_of_timesteps, 1)
 
-                # Get lstm cell output
+                lstm_fw_cell = rnn.BasicLSTMCell(self.number_of_hidden, forget_bias=1.0)
+                lstm_bw_cell = rnn.BasicLSTMCell(self.number_of_hidden, forget_bias=1.0)
+
                 outputs, _, _ = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x, dtype=tf.float32)
 
-                # Linear activation, using rnn inner loop last output
-                return tf.matmul(outputs[4], weights['out']) + biases['out']
+            with tf.name_scope('output'):
+                y_ = tf.matmul(outputs[self.number_of_timesteps // 2], w1) + b1
+                probs = tf.nn.softmax(y_)
 
-            y_ = BiRNN(self.x, w, b)  # logits
-            prediction = tf.nn.softmax(y_)
+            with tf.name_scope('loss'):
+                loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_, labels=self.y))
+                tf.summary.scalar("loss", loss_op)
 
-            # Define loss and optimizer
-            loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_, labels=self.y))
-            optimizer = tf.train.AdamOptimizer()
-            # optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-            self.train_step = optimizer.minimize(loss_op)
+            with tf.name_scope('train'):
+                # optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+                self.train_step = tf.train.AdamOptimizer().minimize(loss_op)
 
-            # Evaluate model (with test y_, for dropout to be disabled)
-            correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(self.y, 1))
-            self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+            correct_pred = tf.equal(tf.argmax(probs, 1), tf.argmax(self.y, 1))
 
-            self.predict = tf.argmax(prediction, 1)
+            with tf.name_scope('accuracy'):
+                self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+                tf.summary.scalar("accuracy", self.accuracy)
 
-            predictions = tf.argmax(prediction, 1)
+            self.predict = tf.argmax(probs, 1)
+
+            predictions = tf.argmax(probs, 1)
             actuals = tf.argmax(self.y, 1)
 
             ones_like_actuals = tf.ones_like(actuals)
@@ -60,17 +62,25 @@ class Model(object):
             ones_like_predictions = tf.ones_like(predictions)
             zeros_like_predictions = tf.zeros_like(predictions)
 
-            self.tp = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(actuals, ones_like_actuals),
-                                                           tf.equal(predictions, ones_like_predictions)), "float"))
+            with tf.name_scope('scores'):
+                self.tp = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(actuals, ones_like_actuals),
+                                                               tf.equal(predictions, ones_like_predictions)), "float"))
+                tf.summary.scalar("tp", self.tp)
+                self.tn = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(actuals, zeros_like_actuals),
+                                                               tf.equal(predictions, zeros_like_predictions)), "float"))
+                tf.summary.scalar("tn", self.tn)
+                self.fp = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(actuals, zeros_like_actuals),
+                                                               tf.equal(predictions, ones_like_predictions)), "float"))
+                tf.summary.scalar("fp", self.fp)
+                self.fn = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(actuals, ones_like_actuals),
+                                                               tf.equal(predictions, zeros_like_predictions)), "float"))
+                tf.summary.scalar("fn", self.fn)
 
-            self.tn = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(actuals, zeros_like_actuals),
-                                                           tf.equal(predictions, zeros_like_predictions)), "float"))
-
-            self.fp = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(actuals, zeros_like_actuals),
-                                                           tf.equal(predictions, ones_like_predictions)), "float"))
-
-            self.fn = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(actuals, ones_like_actuals),
-                                                           tf.equal(predictions, zeros_like_predictions)), "float"))
+            self.summary_op = tf.summary.merge_all()
+            self.writer = tf.summary.FileWriter("logs/{time}-{number_of_hidden}-{number_of_timesteps}".format(
+                time=datetime.datetime.now(),
+                number_of_hidden=self.number_of_hidden, number_of_timesteps=self.number_of_timesteps),
+                graph=self.graph)
 
     def train(self):
         with tf.Session(graph=self.graph) as sess:
@@ -79,20 +89,23 @@ class Model(object):
 
             for e in range(100):
                 print("Epoch {e}".format(e=e))
-
                 print("# of batches {n}".format(n=self.batcher.get_number_of_batches()))
 
                 self.batcher.reset_batches()
+                losses = []
                 while self.batcher.has_more_batches():
                     batch_x, batch_y = self.batcher.get_next_batch()
-                    sess.run(self.train_step, feed_dict={self.x: batch_x, self.y: batch_y})
+                    sum, batch_loss = sess.run([self.summary_op, self.train_step], feed_dict={self.x: batch_x, self.y: batch_y})
+                    losses.append(batch_loss)
 
                 # Test trained model
                 self.test_batcher.reset_batches()
                 batch_x, batch_y = self.test_batcher.get_all()
 
-                print(sess.run([self.accuracy, self.tp, self.tn, self.fp, self.fn],
-                               feed_dict={self.x: batch_x, self.y: batch_y}))
+                sum, acc, tp, tn, fp, fn = sess.run([self.summary_op, self.accuracy, self.tp, self.tn, self.fp,
+                                                     self.fn], feed_dict={self.x: batch_x, self.y: batch_y})
+                self.writer.add_summary(sum, global_step=e)
+                print(acc, tp, tn, fp, fn)
 
                 saver = tf.train.Saver()
                 saver.save(sess, "data/model/epoch", global_step=e)
